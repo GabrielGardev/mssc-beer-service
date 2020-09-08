e20839f2a2a6870dccb85b576228027a09061471
GabrielGardev <gabriel.gurdev@gmail.com>
2020-09-08 12:17:36 +0300

    adding Mapstruct

---
+++ src/main/java/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,14 @@
+package msscbeerservice.web.mappers;
+
+
+import msscbeerservice.domain.Beer;
+import msscbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDto dto);
+}
