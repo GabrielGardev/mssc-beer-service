f2695d78b9cca007f7706abf48f7350455ed7594
GabrielGardev <gabriel.gurdev@gmail.com>
2020-09-10 16:51:35 +0300

    initializing data via data.sql

--- src/main/java/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/msscbeerservice/bootstrap/BeerLoader.java
@@ -3,11 +3,10 @@ package msscbeerservice.bootstrap;
 import msscbeerservice.domain.Beer;
 import msscbeerservice.repositories.BeerRepository;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
-@Component
+//@Component
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
