48063cfcdc3170ed7a26b57e27bf9501168b5896
GabrielGardev <gabriel.gurdev@gmail.com>
2020-09-14 12:02:41 +0300

    update pom.xml

--- target/generated-sources/annotations/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-09-14T10:43:03+0300",
+    date = "2020-09-14T11:50:55+0300",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 14.0.1 (Oracle Corporation)"
 )
 @Component
