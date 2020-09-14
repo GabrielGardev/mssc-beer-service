260e912f65158e9e8c3bcccc0553eccb3a30e44a
GabrielGardev <gabriel.gurdev@gmail.com>
2020-09-14 11:03:13 +0300

    change java and spring versions

--- target/generated-sources/annotations/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-09-13T11:02:50+0300",
+    date = "2020-09-14T10:43:03+0300",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 14.0.1 (Oracle Corporation)"
 )
 @Component
--- target/generated-sources/annotations/msscbeerservice/web/mappers/BeerMapperImpl_.java
+++ target/generated-sources/annotations/msscbeerservice/web/mappers/BeerMapperImpl_.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-09-13T11:02:50+0300",
+    date = "2020-09-14T10:43:02+0300",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 14.0.1 (Oracle Corporation)"
 )
 @Component
